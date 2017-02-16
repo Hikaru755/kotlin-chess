7b6c2f6d44f28cd9b5c3a6f3330c9a4c9c513f9b
Robin Roschlau <robin.roschlau@gmail.com>
2017-02-16 22:31:02 +0100

    :white_check_mark: Add test for king reachable fields test

--- src/test/kotlin/ChessPiecesTest.kt
+++ src/test/kotlin/ChessPiecesTest.kt
@@ -1,10 +1,23 @@
 import com.rrpictureproductions.chess.figures.Color
+import com.rrpictureproductions.chess.figures.King
 import com.rrpictureproductions.chess.figures.Knight
 import com.rrpictureproductions.chess.toPosition
 import org.junit.Test
 
 
 class ChessPiecesTest {
+
+    @Test
+    fun testReachablePositionsKing() {
+        val king = King(Color.WHITE)
+
+        val actual = king.getReachablePositionsFrom("D4".toPosition())
+        val expected = setOf("C3", "C4", "C5", "D3", "D5", "E3", "E4", "E5").map(String::toPosition).toSet()
+        assertEquals(expected, actual)
+
+        assertEquals(expected = king.getReachablePositionsFrom("H1".toPosition()),
+                     actual = setOf("G1", "G2", "H2").map(String::toPosition).toSet())
+    }
 
     @Test
     fun testReachablePositionsKnight() {
